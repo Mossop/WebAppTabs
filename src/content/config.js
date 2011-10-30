06e882e508f4fce0b46c62d3b30c1bcf9af416e0
Dave Townsend <dtownsend@oxymoronical.com>
2011-10-30 12:57:27 -0700

    Add drag and drop reordering of webapps from the config UI. Closes #9

--- src/content/config.js
+++ src/content/config.js
@@ -74,9 +74,9 @@ const config = {
     document.getElementById("txt_name").value = "";
     document.getElementById("txt_href").value = "";
 
-    this.addWebAppItem(desc);
     ConfigManager.webappList.push(desc);
     ConfigManager.persistPrefs();
+    this.addWebAppItem(desc);
   },
 
   remove: function() {
@@ -95,6 +95,119 @@ const config = {
 
   select: function() {
     document.getElementById("btn_remove").disabled = !this.list.selectedItem;
+  },
+
+  findTargetForDrop: function(aEvent) {
+    let target = aEvent.target;
+    let source = aEvent.dataTransfer.mozSourceNode;
+
+    // If dropping on the list then we want to drop at the end of the list
+    if (target == this.list)
+      return null;
+
+    let rect = target.getBoundingClientRect();
+    // If dropping closer to the top of the element then drop before the element
+    if ((aEvent.clientY - rect.top) < (rect.bottom - aEvent.clientY))
+      return target;
+    // Otherwise drop after
+    return target.nextSibling;
+  },
+
+  dragStart: function(aEvent) {
+    let item = aEvent.target;
+    if (item.localName != "richlistitem")
+      return;
+    let desc = item.desc;
+
+    let dt = aEvent.dataTransfer;
+    dt.setData("application/x-webapptab", desc.id);
+    dt.addElement(item);
+
+    let rect = item.getBoundingClientRect();
+    dt.setDragImage(item, (rect.right - rect.left) / 2, (rect.bottom - rect.top) / 2);
+
+    dt.effectAllowed = "move";
+  },
+
+  dragEnter: function(aEvent) {
+    let dt = aEvent.dataTransfer;
+
+    if (this.lastDragTarget)
+      this.lastDragTarget.removeAttribute("dragpos");
+
+    // Don't allow drops from anywhere else
+    if (!dt.types.contains("application/x-webapptab"))
+      return;
+
+    let sourceItem = dt.mozSourceNode;
+    // Dropping an item onto itself does nothing
+    if (sourceItem == aEvent.target)
+      return;
+
+    let targetItem = this.findTargetForDrop(aEvent);
+
+    // Can't drop before or after the original item
+    if (targetItem == sourceItem || targetItem == sourceItem.nextSibling)
+      return;
+
+    this.list.setAttribute("dragging", "true");
+    if (targetItem) {
+      targetItem.setAttribute("dragpos", "before");
+      this.lastDragTarget = targetItem;
+    }
+    else {
+      this.list.lastChild.setAttribute("dragpos", "after");
+      this.lastDragTarget = this.list.lastChild;
+    }
+
+    aEvent.preventDefault();
+  },
+
+  dragOver: function(aEvent) {
+    this.dragEnter(aEvent);
+  },
+
+  drop: function(aEvent) {
+    let dt = aEvent.dataTransfer;
+
+    // Don't allow drops from anywhere else
+    if (!dt.types.contains("application/x-webapptab"))
+      return;
+
+    let sourceItem = dt.mozSourceNode;
+    // Dropping an item onto itself does nothing
+    if (sourceItem == aEvent.target)
+      return;
+
+    let targetItem = this.findTargetForDrop(aEvent);
+
+    // Can't drop before or after the original item
+    if (targetItem == sourceItem || targetItem == sourceItem.nextSibling)
+      return;
+
+    this.list.insertBefore(sourceItem, targetItem);
+
+    let sourcePos = ConfigManager.webappList.indexOf(sourceItem.desc);
+    ConfigManager.webappList.splice(sourcePos, 1);
+
+    if (targetItem) {
+      let targetPos = ConfigManager.webappList.indexOf(targetItem.desc);
+      ConfigManager.webappList.splice(targetPos, 0, sourceItem.desc);
+    }
+    else {
+      ConfigManager.webappList.push(sourceItem.desc);
+    }
+    ConfigManager.persistPrefs();
+
+    aEvent.preventDefault();
+  },
+
+  dragEnd: function(aEvent) {
+    if (this.lastDragTarget)
+      this.lastDragTarget.removeAttribute("dragpos");
+    this.lastDragTarget = null;
+
+    this.list.removeAttribute("dragging");
   },
 
   addWebAppItem: function(aDesc) {
