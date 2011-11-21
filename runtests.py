cba2a983f6d962f170f01b629e0f0326cdce97eb
Dave Townsend <dtownsend@oxymoronical.com>
2011-11-20 18:44:02 -0800

    Add extension to serve as a simple test harness and python script to launch and interpret the results

---
+++ runtests.py
@@ -0,0 +1,193 @@
+#! python
+
+import sys, os
+
+basedir = os.path.abspath(os.path.dirname(sys.argv[0]))
+sys.path.append(os.path.join(basedir, 'mozbase/mozprocess'))
+sys.path.append(os.path.join(basedir, 'mozbase/mozinfo'))
+sys.path.append(os.path.join(basedir, 'mozbase/manifestdestiny'))
+sys.path.append(os.path.join(basedir, 'mozbase/mozprofile'))
+sys.path.append(os.path.join(basedir, 'mozbase/mozrunner'))
+
+from manifestparser import TestManifest
+from mozprocess import ProcessHandlerMixin
+from mozprofile import ThunderbirdProfile
+from mozrunner import ThunderbirdRunner
+import json
+
+class CommandFile():
+  commands = []
+
+  def addCommand(self, command, arguments=[]):
+    self.commands.append({
+      'command': command,
+      'args': arguments
+    })
+
+  def writeCommands(self, profile):
+    file = open(os.path.join(profile.profile, 'commands.json'), 'w')
+    file.write(json.dumps(self.commands))
+    file.close()
+    
+class TestManager():
+  passCount = 0
+  failCount = 0
+  sawQuit = False
+
+  def __init__(self, profile, runner, tests):
+    self.profile = profile
+    self.runner = runner
+    self.tests = tests
+
+  def runTests(self):
+    while len(self.tests) > 0:
+      commands = CommandFile()
+      while len(self.tests) > 0:
+        test = self.tests[0]
+        commands.addCommand('runTest', [test['path']])
+        self.tests = self.tests[1:]
+        if test['restart'] == 'true':
+          break
+      commands.writeCommands(self.profile)
+
+      self.sawQuit = False
+      self.runner.start()
+      self.runner.wait()
+      if not self.sawQuit:
+        self.processFailLine("Unexpected application exit")
+    print("\n%d tests passed, %d tests failed" % (self.passCount, self.failCount))
+
+  def processOutputLine(self, line):
+    if line[0:9] == "!!!PASS: ":
+      self.processPassLine(line[9:])
+    elif line[0:9] == "!!!FAIL: ":
+      self.processFailLine(line[9:])
+    elif line[0:9] == "!!!INFO: ":
+      self.processInfoLine(line[9:])
+    else:
+      self.processUnknownLine(line)
+
+  def processPassLine(self, line):
+    self.passCount += 1
+    print("TEST-PASS %s" % line)
+
+  def processFailLine(self, line):
+    self.failCount += 1
+    print("TEST-FAIL %s" % line)
+
+  def processInfoLine(self, line):
+    if line == "TEST-QUIT":
+      self.sawQuit = True
+      return
+    print(line)
+
+  def processUnknownLine(self, line):
+    print("   %s" % line)
+
+class TestProcess(ProcessHandlerMixin):
+  def __init__(self, cmd, **kwargs):
+    kwargs.setdefault('processOutputLine', []).append(manager.processOutputLine)
+    ProcessHandlerMixin.__init__(self, cmd, **kwargs)
+
+class TestProfile(ThunderbirdProfile):
+  preferences = {
+    # Turn on debug logging
+    'extensions.webapptabs.loglevel': 0,
+    # Don't automatically update the application
+    'app.update.enabled': False,
+    # Only install add-ons from the profile and the application scope
+    # Also ensure that those are not getting disabled.
+    # see: https://developer.mozilla.org/en/Installing_extensions
+    'extensions.enabledScopes': 5,
+    'extensions.autoDisableScopes': 10,
+    # Don't install distribution add-ons from the app folder
+    'extensions.installDistroAddons': False,
+    # Dont' run the add-on compatibility check during start-up
+    'extensions.showMismatchUI': False,
+    # Don't automatically update add-ons
+    'extensions.update.enabled': False,
+    # say yes to debug output via dump
+    'browser.dom.window.dump.enabled': True,
+    # say no to slow script warnings
+    'dom.max_chrome_script_run_time': 0,
+    'dom.max_script_run_time': 0,
+    # do not ask about being the default mail client
+    'mail.shell.checkDefaultClient': False,
+    # do not tell us about the greatness that is mozilla (about:rights)
+    'mail.rights.override': True,
+    # disable non-gloda indexing daemons
+    'mail.winsearch.enable': False,
+    'mail.winsearch.firstRunDone': True,
+    'mail.spotlight.enable': False,
+    'mail.spotlight.firstRunDone': True,
+    # disable address books for undisclosed reasons
+    'ldap_2.servers.osx.position': 0,
+    'ldap_2.servers.oe.position': 0,
+    # disable the first use junk dialog
+    'mailnews.ui.junk.firstuse': False,
+    # Do not allow check new mail to be set
+    'mail.startup.enabledMailCheckOnce': True,
+    # Dont load whats new or the remote start page - keep everything local
+    # under our control.
+    'mailnews.start_page_override.mstone': 'ignore',
+    'mailnews.start_page.url': 'about:blank',
+    # Do not enable gloda
+    'mailnews.database.global.indexer.enabled': False,
+    # But do have gloda log if it does anything.  (When disabled, queries
+    # are still serviced; they just should not result in any matches.)
+    'mailnews.database.global.logging.upstream': True,
+    # Do not allow fonts to be upgraded
+    'mail.font.windows.version': 2,
+    # No, we dont want to be prompted about Telemetry
+    'toolkit.telemetry.prompted': True,
+    # Create fake accounts so the new account wizard won't open
+    'mail.account.account1.server': 'server1',
+    'mail.account.account2.identities': 'id1,id2',
+    'mail.account.account2.server': 'server2',
+    'mail.accountmanager.accounts': 'account1,account2',
+    'mail.accountmanager.defaultaccount': 'account2',
+    'mail.accountmanager.localfoldersserver': 'server1',
+    'mail.identity.id1.fullName': 'Tinderbox',
+    'mail.identity.id1.htmlSigFormat': False,
+    'mail.identity.id1.htmlSigText': 'Tinderbox is soo 90ies',
+    'mail.identity.id1.smtpServer': 'smtp1',
+    'mail.identity.id1.useremail': 'tinderbox@invalid.com',
+    'mail.identity.id1.valid': True,
+    'mail.identity.id2.fullName': 'Tinderboxpushlog',
+    'mail.identity.id2.htmlSigFormat': True,
+    'mail.identity.id2.htmlSigText': 'Tinderboxpushlog is the new <b>hotness!</b>',
+    'mail.identity.id2.smtpServer': 'smtp1',
+    'mail.identity.id2.useremail': 'tinderboxpushlog@invalid.com',
+    'mail.identity.id2.valid': True,
+    'mail.server.server1.directory-rel': '[ProfD]Mail/Local Folders',
+    'mail.server.server1.hostname': 'Local Folders',
+    'mail.server.server1.name': 'Local Folders',
+    'mail.server.server1.type': 'none',
+    'mail.server.server1.userName': 'nobody',
+    'mail.server.server2.check_new_mail': False,
+    'mail.server.server2.directory-rel': '[ProfD]Mail/tinderbox',
+    'mail.server.server2.download_on_biff': True,
+    'mail.server.server2.hostname': 'tinderbox',
+    'mail.server.server2.login_at_startup': False,
+    'mail.server.server2.name': 'tinderbox@invalid.com',
+    'mail.server.server2.type': 'pop3',
+    'mail.server.server2.userName': 'tinderbox',
+    'mail.server.server2.whiteListAbURI': '',
+    'mail.smtp.defaultserver': 'smtp1',
+    'mail.smtpserver.smtp1.hostname': 'tinderbox',
+    'mail.smtpserver.smtp1.username': 'tinderbox',
+    'mail.smtpservers': 'smtp1',
+  }
+
+addons = [
+  os.path.join(basedir, 'src'),
+  os.path.join(basedir, 'testharness')
+]
+
+manifest = TestManifest([os.path.join(basedir, 'tests/tests.ini')])
+tests = manifest.active_tests(disabled=False)
+profile = TestProfile(addons=addons)
+runner = ThunderbirdRunner(profile=profile, process_class=TestProcess)
+manager = TestManager(profile, runner, tests)
+
+manager.runTests()
---
+++ testharness/testharness.js
@@ -0,0 +1,234 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ *   Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is WebApp Tabs.
+ *
+ * The Initial Developer of the Original Code is
+ * Dave Townsend <dtownsend@oxymoronical.com>
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import("resource://gre/modules/Services.jsm");
+Components.utils.import("resource://gre/modules/NetUtil.jsm");
+Components.utils.import("resource://gre/modules/FileUtils.jsm");
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Ce = Components.Exception;
+const Cr = Components.results;
+
+function TestFunctions(aHarness, aScriptURL) {
+  this.harness = aHarness;
+  this.script = aScriptURL;
+}
+
+TestFunctions.prototype = {
+  harness: null,
+  script: null,
+
+  info: function(aMessage) {
+    this.harness.log("TEST-INFO " + aMessage);
+  },
+
+  ok: function(aCondition, aMessage) {
+    if (aCondition)
+      this.harness.logPass(aMessage);
+    else
+      this.harness.logFail(aMessage);
+  },
+
+  is: function(aFound, aExpected, aMessage) {
+    if (aFound == aExpected)
+      this.harness.logPass(aMessage);
+    else
+      this.harness.logFail(aMessage);
+  },
+
+  isnot: function(aFound, aNotExpected, aMessage) {
+    if (aFound != aNotExpected)
+      this.harness.logPass(aMessage);
+    else
+      this.harness.logFail(aMessage);
+  }
+};
+
+function TestHarness() {
+}
+
+TestHarness.prototype = {
+  window: null,
+  commandList: null,
+
+  log: function(aStr) {
+    dump("!!!INFO: " + aStr + "\n");
+  },
+
+  logPass: function(aStr) {
+    dump("!!!PASS: " + aStr + "\n");
+  },
+
+  logFail: function(aStr, aException) {
+    dump("!!!FAIL: " + aStr + "\n");
+    if (aException)
+      dump("!!!INFO:           Exception: " + aException + "\n");
+  },
+
+  // All of these are run as if they are members of TestHarness
+  commands: {
+    runTest: function(aTestPath) {
+      this.log("TEST-START " + aTestPath);
+
+      let file = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+      file.initWithPath(aTestPath);
+      let url = NetUtil.newURI(file);
+
+      let self = this;
+      let sandbox = this.createTestSandbox(url, function() {
+        self.log("TEST-END " + aTestPath);
+        self.runNextCommand();
+      });
+
+      try {
+        sandbox.waitForExplicitFinish();
+        sandbox.test();
+      }
+      catch (e) {
+        this.logFail("Exception running test", e);
+      }
+      finally {
+        sandbox.finish();
+      }
+    }
+  },
+
+  createTestSandbox: function(aScriptURL, aFinishedCallback) {
+    let args = {
+      sandboxName: aScriptURL,
+      sandboxPrototype: this.window
+    };
+
+    let sandbox = Components.utils.Sandbox(this.window, args);
+
+    let functions = new TestFunctions(this, aScriptURL);
+    ["info", "ok", "is", "isnot"].forEach(function(aFunc) {
+      sandbox[aFunc] = functions[aFunc].bind(functions);
+    });
+
+    let self = this;
+    let pending = 0;
+    sandbox.waitForExplicitFinish = function() {
+      pending++;
+    }
+    sandbox.finish = function() {
+      if (--pending == 0)
+        aFinishedCallback();
+    }
+
+    try {
+      Components.utils.evalInSandbox(
+        "Components.classes['@mozilla.org/moz/jssubscript-loader;1']" +
+                  ".createInstance(Components.interfaces.mozIJSSubScriptLoader)" +
+                  ".loadSubScript('" + aScriptURL.spec + "');", sandbox, "ECMAv5");
+    }
+    catch (e) {
+      this.logFail("Exception loading script " + aScriptURL, e);
+    }
+
+    return sandbox
+  },
+
+  loadCommands: function() {
+    let fis = Cc["@mozilla.org/network/file-input-stream;1"].
+              createInstance(Ci.nsIFileInputStream);
+    let json = Cc["@mozilla.org/dom/json;1"].
+               createInstance(Ci.nsIJSON);
+
+    try {
+      let file = FileUtils.getFile("ProfD", ["commands.json"], false);
+      fis.init(file, -1, 0, 0);
+      this.commandList = json.decodeFromStream(fis, file.fileSize);
+    }
+    catch (e) {
+      this.logFail("Failed to read commands", e);
+      this.commandList = [];
+    }
+    finally {
+      fis.close();
+    }
+  },
+
+  runCommands: function() {
+    this.loadCommands();
+    this.runNextCommand();
+  },
+
+  runNextCommand: function() {
+    if (this.commandList.length == 0) {
+      this.log("TEST-QUIT");
+      Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup)
+                                              .quit(Ci.nsIAppStartup.eAttemptQuit);
+      return;
+    }
+
+    let command = this.commandList.shift();
+    if (!(command.command in this.commands)) {
+      this.logFail("Unsupported harness command " + command.command);
+      this.runNextCommand();
+      return;
+    }
+
+    try {
+      this.commands[command.command].apply(this, command.args);
+    }
+    catch (e) {
+      this.logFail("Exception running command " + command.command, e);
+      this.runNextCommand();
+    }
+  },
+
+  observe: function(aSubject, aTopic, aData) {
+    switch (aTopic) {
+    case "profile-after-change":
+      Services.obs.addObserver(this, "mail-startup-done", false);
+      break;
+    case "mail-startup-done":
+      Services.obs.removeObserver(this, "mail-startup-done");
+      this.window = aSubject.QueryInterface(Ci.nsIDOMWindowInternal);
+      // Hacky but does the job
+      this.window.setTimeout(this.runCommands.bind(this), 100);
+      break;
+    }
+  },
+
+  classID: Components.ID("{ff8e5752-07f1-40c9-9491-064d23f2edcd}"),
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver])
+};
+
+var NSGetFactory = XPCOMUtils.generateNSGetFactory([TestHarness]);
